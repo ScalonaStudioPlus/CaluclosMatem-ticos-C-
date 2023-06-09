44ea8ea029fec71e6e39be48d0ad85e869b22ec5
Jhon Escalona <116379627+ScalonaStudioPlus@users.noreply.github.com>
2023-06-09 23:15:34 +0200

    Add files via upload

---
+++ CalculosMatemaricos.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+int main(){
+	int largo, ancho;
+	int perimetro, area;
+	
+	std::cout << "Ingresa el largo: ";
+	std::cin >> largo;
+	std::cout << "Ingresa el ancho ";
+	std::cin >> ancho;
+	
+	perimetro = largo * 2 + ancho * 2;
+	area = largo * ancho;
+	
+	std::cout << "Perimetro= " << perimetro << std::endl;
+	std::cout << "Area= " << area;
+}
